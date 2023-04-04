18d3d9159a7e03252452cedcaa075d1d3ce29090
duyensherry <dinhhueduyen@gmail.com>
2023-04-04 23:42:04 +0700

    baitaptuan3

---
+++ week3.1.js
@@ -0,0 +1,20 @@
+var name1 = 'Dinh' + ' ' + 'Thi' + ' ' + 'Hue' + ' ' + 'Duyen';
+console.log('My name is' + ' ' + name1 + '.');
+//
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c + ' ' + 'is sum of' + ' ' + a + ' ' + 'and' + ' ' + b + '.');
+//
+var width = 10;
+var height = 20;
+var area = width * height;
+console.log('Area of retangle with width' + ' ' + width + ' ' + 'and height' + ' ' + height + ' ' + 'is' + ' ' + area + '.');
+//
+var isSuccess = area > 100;
+console.log('Area of the above retangle is over 100. The result is' + ' ' + isSuccess + '.');
+//
+var m = 5;
+var n = ++m + 1 - m++;
+var p = n + (m--);
+console.log(p,n);
