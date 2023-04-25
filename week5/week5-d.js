23e6214c8739fe71b21d6c4e766d08e6946e442c
duyensherry <dinhhueduyen@gmail.com>
2023-04-26 00:18:09 +0700

    resubmit_exercise_week_5

--- week5/week5-d.js
+++ week5/week5-d.js
@@ -1,13 +1,8 @@
-/**     console.log(fullName.split(' ')[0] + ' '+ fullName.split(' ')[1].toUpperCase());
-
+/**
  * Viết hàm trả về họ tên in hoa firstName trong họ tên của bạn. Tham số đầu vào là họ tên
  * eg: var result = getCharactersInName("Dinh Duyen") => result will be: Dinh DUYEN
- * 
- * 
-console.log(fullName.split(' ')[0] + ' '+ fullName.split(' ')[fullName.split(' ').length - 1].toUpperCase());
 * */
 
- 
 function getCharactersInName(fullName) {
     var nameInArray = fullName.split(' ');
     var firstName = nameInArray[nameInArray.length - 1];
@@ -16,5 +11,3 @@ function getCharactersInName(fullName) {
 }
 var result = getCharactersInName('Dinh Thi Hue Duyen');
 console.log(result);
-
-
