c8cbdc0f05d8cbbd4dc2a98abb1b43136ef97608
minology <bluewings2605@gmail.com>
2023-05-24 20:24:29 +0700

    assign revision exercises

---
+++ week6/a.js
@@ -0,0 +1,5 @@
+/**
+ * Write a function compare 2 string. Receive 2 parameters are string1, string2 and compare those 2 strings, not case sensitive. Result will be boolean
+ * Eg: compareString("aBc", "aBC") -> true
+ * Eg: compareString("aBc", "aBdC") -> false
+ * */
---
+++ week6/b.js
@@ -0,0 +1,8 @@
+/**
+ * Write a function return day in week by index. Receive a parameter as index. Result will be day in week.
+ * Eg: dayInWeek(1) -> Monday
+ * Eg: dayInWeek(2) -> Tuesday
+ * Eg: dayInWeek(7) -> Sunday
+ * Eg: dayInWeek(8) -> Monday
+ * Eg: dayInWeek(10) -> Wednesday
+ * */
