b89cd9d74a62e3099bc426ff71cdfc8085bc8dc4
minology <bluewings2605@gmail.com>
2023-06-03 09:01:21 +0700

    Assign exercise

---
+++ week7/a.js
@@ -0,0 +1,9 @@
+/**
+ * Write a function check month in year. Receive a parameter is a number and return the month in year. If receive invalid input, return "INVALID INPUT". Using SWITCH
+ * eg: checkMonth(1) -> January
+ * eg: checkMonth(7) -> July
+ * eg: checkMonth(12) -> December
+ * eg: checkMonth(13) -> 'INVALID INPUT'
+ * eg: checkMonth(-1) -> 'INVALID INPUT'
+ * eg: checkMonth(abc) -> 'INVALID INPUT'
+ * */
---
+++ week7/b.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function check hour in a day. Recive a parameter as hour in a day and return correct type of hour (AM or PM). Invalid hour will return null. Must use TENARY OPERATOR somewhere!
+ * eg: checkHour(0) -> 0:00 AM
+ * eg: checkHour(5) -> 5:00 AM
+ * eg: checkHour(12) -> 5:00 AM
+ * eg: checkHour(15) -> 15:00 PM
+ * eg: checkHour(24) -> null
+ * eg: checkHour(25) -> null
+ * eg: checkHour(-1) -> null
+ * eg: checkHour(abc) -> null
+ * eg: checkHour(5.5) -> null
+ * */
