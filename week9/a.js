a32b92dfbdedbadceb555b68416cfd952e911205
duyensherry <dinhhueduyen@gmail.com>
2023-06-26 22:17:47 +0700

    exercise_week_9

--- week9/a.js
+++ week9/a.js
@@ -1,3 +1,16 @@
 /**
  * Write a function receive an array of numbers. Find the maximum number in the given array. MUST USE LOOP!
  * */
+function findMaxNumber() {
+    var max = 0;
+    var arrayOfNumber = [1, 9, 3, 14];
+    for (var key in arrayOfNumber) {
+        if (arrayOfNumber[key] > max) {
+            max = arrayOfNumber[key];
+            key++;
+        }
+    }      
+    return max; 
+}
+
+console.log(findMaxNumber());
--- week9/c.js
+++ week9/c.js
@@ -1,5 +1,32 @@
-/**
- * Write a function receive an array of numbers. Return all the even numbers between the minimum number and maximum number as an array.
- * Example: findEvenNumbersInRange([1,4,10,3]) => Result will be [2,4,6,8]
- * Explain: 1 is the min, 10 is Max. Get all even numbers between 1 and 10
- * */
+var stringOfNumbers = [1, 3, 0, 4];
+
+ 
+
+function findEvenNumber() {
+
+    function findMaxNumber() {
+        var max = 0;
+        for (key in stringOfNumbers) {
+            if (stringOfNumbers[key] > max) {
+                max = stringOfNumbers[key];
+            }
+        }
+        return max;
+    }
+
+    function findMinNumber() {
+        var min = stringOfNumbers[0];
+        for (key in stringOfNumbers) {
+            if (stringOfNumbers[key] < min) {
+                min = stringOfNumbers[key];
+            }
+        }
+        return min;
+    }
+    var num = 0;
+    while(num%2 == 0 && num <= 2) {
+    console.log(num);
+    num += 2;
+    }
+}
+findEvenNumber()
